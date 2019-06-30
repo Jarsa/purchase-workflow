c905bbd5ec82ae996979ccac0bd34a4621c4fe76
lmgr0312 <lmiguel.gruiz@gmail.com>
2019-06-30 17:43:35 -0500

    [ADD][Purchase Order]Add department field in purchase order to use in access record rules

--- purchase_request_department/__manifest__.py
+++ purchase_request_department/__manifest__.py
@@ -9,7 +9,10 @@
     "category": "Purchase Management",
     "post_init_hook": "post_init_hook",
     "depends": ["hr", "purchase_request"],
-    "data": ["views/purchase_request_department_view.xml"],
-    "license": "LGPL-3",
+    "data": [
+        "views/purchase_request_department_view.xml",
+        "views/purchase_order_department_view.xml",
+    ],
+    "license": 'LGPL-3',
     "installable": True,
 }
